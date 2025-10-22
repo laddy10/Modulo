5c97f3a5fa4e7d92e2f45412440c75ff611641cb
Laddy10 <laddy.sofi@gmail.com>
2025-10-22 08:48:14 -0500

    Actualizacion

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Usuario\Desktop\KARENCITA_PRUEBAS\modulo_gestion\env"
+set "VIRTUAL_ENV=D:\Modulo_Gestion\modulo_gestion\env"
 
 if not defined PROMPT set PROMPT=$P$G
 
